ae6574d2884eb755925d6854ae02dd9c7e4bb04a
Bharath200415 <bharath200415@gmail.com>
2026-03-28 13:56:53 +0530

    updates

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -94,12 +94,14 @@ router.get("/bulk",async (req,res)=>{
     const users = await User.find({
         $or: [{
             firstName:{
-                "$regex":filter
+                "$regex":filter,
+                "$options": "i"
             }
             
         },{
             lastName:{
-                "$regex":filter
+                "$regex":filter,
+                "$options": "i"
 
             }
         }]
